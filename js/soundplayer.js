7b0f783c602b5c9f8ca55f4d043a19983ae33fdd
Luke Sturgeon <lukesturgeon@Lukes-MacBook-Pro-3.local>
2016-03-26 15:42:50 +0000

    added compass detection to location marker

---
+++ js/soundplayer.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var SoundPlayer = function(){
+	
+	/* public methods and variables */
+	
+	this.ended = true;
+	
+	this.queue = [];
+	
+	this.audio = new Audio();	
+	
+	this.onended = function(e){
+		if (this.queue.length > 0) {
+			var file = this.queue.shift();
+			this._play_sound( file );
+		}
+	};
+	
+	this.play_sound = function( _file ){
+		
+		if ( this.ended ) {
+			console.log( "play new sound" );
+			this._play_sound( _file );
+		}
+		else {
+			console.log( "queue up a sound " + _file );
+			this.queue.push( _file );
+		}
+		
+	}
+	
+	/* events and triggers */
+	
+	this.audio.addEventListener("ended", this.onended.bind(this), false);
+	
+	/* private methods */
+	
+	this._play_sound = function( _file ){
+		this.ended = false;
+		this.audio.src = _file;
+		this.audio.play();
+	}
+	
+};
