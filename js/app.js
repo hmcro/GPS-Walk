f33158bd9e48e9efe76042a024c8c4f5582e7db5
Luke <hello@lukesturgeon.co.uk>
2018-09-10 15:21:32 +0100

    removed old js and stick to jquery

--- js/app.js
+++ js/app.js
@@ -1,233 +1,234 @@
-var app = new Vue(
-{
-	el:"#app",
+var app = {
+
+	audio: new Audio(),
+	isAudioPlaying: false,
+	mapbox: null,
+	lngLat: [0,0],
+	markers: [],
+	userMarker: null,
 	
-	data:{
-		state: "welcome",
-		geolocationSettings: {
-			enableHighAccuracy: false, 
-			maximumAge        : 0, 
-			timeout           : 20000
-		},
-		geolocationId: null,
-		map: null,
-		userMarker: null,
-		audio: new Audio(),
-		audioName: "",
-		currentTime: 0,
-		totalTime: 0,
-		isPlaying: false,
+    init: function( settings ) {
+        app.config = {
+            geolocationSettings: {
+				enableHighAccuracy: false, 
+				maximumAge        : 0, 
+				timeout           : 20000
+			},
+			markersUrl: 'https://raw.githubusercontent.com/hmcro/Maps/master/test-n20sx.geojson',
+			mapboxUrl: 'mapbox://styles/lukesturgeon/cjlrt20fu8i3z2spjnowdy5ae',
+			audioDistance: 20
+        };
+ 
+        // Allow overriding the default config
+        $.extend( app.config, settings );
+ 
+        app.setup();
+    },
+ 
+    setup: function() {
+		console.log('hmcro v 3.1r1');
+
+		// hide all pages
+		app.goto();
+
+		// hide the technical warnings
+		if ( Modernizr.audio ) $('#audioSupported').hide();
+		if ( Modernizr.geolocation ) $('#geolocationSupported').hide();
+
+		// setup button events
+		$('a[href="#requirements"]').click(function(){
+			app.goto('#requirements');
+		});
+
+		$('a[href="#map"]').click(function(){
+
+			navigator.geolocation.getCurrentPosition(
+				app.onGeolocationSuccess, 
+				app.onGeolocationError, 
+				app.config.geolocationSettings);
+
+			app.goto('#map');
+			app.playAudio('1_Welcome');
+		});
+
+		$('a[href="#welcome"]').click(function(){
+			app.goto('#welcome');
+		});
+
+		$('a[href="#privacy"]').click(function(){
+			app.goto('#privacy');
+		});
+
+		// setup audio player
+        app.audio.addEventListener("timeupdate", app.onAudioUpdate);
+		app.audio.addEventListener("ended", app.onAudioStopped);
+		app.audio.addEventListener("pause", app.onAudioStopped);
+		app.audio.addEventListener("play", app.onAudioStarted);
+
+		// load data
+		$.ajax({
+			dataType: "json",
+			url: app.config.markersUrl,
+			success: app.onMarkerJson
+		});
 	},
 
-	created: function(){
+	goto: function( hash ){
 
-		console.log('hmcro v 3.0r3');
+		// hide all pages except welcome
+		$('#js-error').hide();
+		$('#welcome').hide();
+		$('#requirements').hide();
+		$('#map').hide();
+		$('#privacy').hide();
 
-		window.addEventListener("hashchange", this.hashChanged);
-
-		this.audio.addEventListener("timeupdate", this.onAudioUpdate);
-		this.audio.addEventListener("ended", this.onAudioStopped);
-		this.audio.addEventListener("pause", this.onAudioStopped);
-		this.audio.addEventListener("play", this.onAudioStarted);
-
-		/* LOAD & PARSE THE GEOSJON */
-		var _this = this;
-		var xobj = new XMLHttpRequest();
-		
-		xobj.overrideMimeType("application/json");
-		xobj.open('GET', 'https://raw.githubusercontent.com/hmcro/Maps/master/test-n20sx.geojson', true);
-		xobj.onreadystatechange = function () {
-			if (xobj.readyState == 4 && xobj.status == "200") {
-				_this.markers = JSON.parse(xobj.responseText);
-			}
-		};
-		xobj.send(null);
+		// show the new page
+		$(hash).show();
 	},
 
-	methods:{
-
-		onHashChanged: function( event ){
-			console.log( event );
-		},
-
-		goto: function(newState){
-
-			if (newState == 'map' && !this.map ) {
-				this.initGeolocation();
-			}
-
-			/** change the page */
-			this.state = newState;
-			
-		},
-
-		link: function(url){
-			location.href = url;
-		},
-
-		/* GEOLOCATION STUFF */
-		
-		initGeolocation: function(){
-
-			/* will trigger a location access request */
-			navigator.geolocation.getCurrentPosition(this.onGeolocationSuccess, this.onGeolocationError, this.geolocationSettings);
-		},
-
-		onGeolocationSuccess: function(pos){
-
-			this.latitude = pos.coords.latitude;
-			this.longitude = pos.coords.longitude;
-
-			console.log(this.longitude, this.latitude);
-
-			if (!this.map) {
-
-				this.initMap();
-
-				this.playAudio( "1_Welcome" );
-			}
-			else {
-				/* update the position of the user */
-				if (this.userMarker) {
-					this.userMarker.setLngLat([this.longitude, this.latitude]);
-				}
-
-				/* slide the map to the next position */
-				this.map.panTo([this.longitude, this.latitude]);
-				
-				/* check the distance between all stories */
-				var _this = this;
-				this.markers['features'].forEach(function(marker) {
-
-					/* measure the distance between the user position and each marker position */
-					var from = turf.point([_this.longitude, _this.latitude]);
-					var to = turf.point([marker.geometry.coordinates[0], marker.geometry.coordinates[1]]);
-
-					/* convert km to nearest metre */				
-					var distance = Math.floor( turf.distance(from, to) * 1000 );
-
-					if ( distance <= 20 ) {
-						_this.playAudio( marker.properties.mp3 );
-						alert("play " + marker.properties.mp3 );
-					}
-				});
-
-
-				// NEED TO REMOVE THE STORY FROM ARRAY
-			}
-		},
-
-		onGeolocationError:function(err){
-			if (err.code == 1) {
-				alert("Location access is denied. Use your phone's settings to enable.");
-			}
-			else {
-				console.warn('ERROR('+err.code+'): ' + err.message);
-				alert( err.message );
-			}			
-		},
-
-		/* MAPBOX STUFF */
-
-		initMap: function() {
-
-			console.log("map init");
-			console.log(this);
-
-			/* will initialise the mapbox */
-			mapboxgl.accessToken = 'pk.eyJ1IjoibHVrZXN0dXJnZW9uIiwiYSI6ImNpazcwenlzYjAwenZpZm0yZGVtOXpzNGoifQ.qBHqidaLVWQtIEu09uhSkg';
-			this.map = new mapboxgl.Map({
-				container: 'map',
-				style: 'mapbox://styles/lukesturgeon/cjlrt20fu8i3z2spjnowdy5ae',
-				center: [this.longitude, this.latitude],
-				zoom: 17.1
+	onMarkerJson: function( data ) {
+		data.features.forEach(function(marker){
+			app.markers.push({
+				lngLat: [
+					marker.geometry.coordinates[0],
+					marker.geometry.coordinates[1]
+				],
+				mp3: marker.properties.mp3
 			});
+			// console.log(marker);
+		});
 
-			var _this = this;
-			this.map.on("load", function(){
+		// now the data has loaded, start the experience
+		app.goto('#welcome');
+	},
+
+	onGeolocationSuccess: function( pos ) {
+		app.lngLat[0] = pos.coords.longitude;
+		app.lngLat[1] = pos.coords.latitude;
+		console.log(app.lngLat);
+
+		if ( !app.mapbox ) {
+			app.setupMapbox();
+		}
+		else {
+			/* update the position of the user */
+			if (app.userMarker) {
+				app.userMarker.setLngLat(app.lngLat);
+			}
+
+			/* slide the map to the next position */
+			app.mapbox.panTo(app.lngLat);
+			
+			app.findMarkerToPlay();
+		}
+	},
+
+	onGeolocationError: function(error) {
+		if (error.code == 1) {
+			alert("Location access is denied. Use your phone's settings to enable.");
+		}
+		else {
+			console.warn('ERROR('+error.code+'): ' + error.message);
+			alert( error.message );
+		}	
+	},
+
+	setupMapbox: function() {
+		console.log("setupMapbox");
+
+		/* will initialise the mapbox */
+		mapboxgl.accessToken = 'pk.eyJ1IjoibHVrZXN0dXJnZW9uIiwiYSI6ImNpazcwenlzYjAwenZpZm0yZGVtOXpzNGoifQ.qBHqidaLVWQtIEu09uhSkg';
+		app.mapbox = new mapboxgl.Map({
+			container: 'mapbox',
+			style: app.config.mapboxUrl,
+			center: app.lngLat,
+			zoom: 17.1
+		});
+
+		app.mapbox.on("load", function(){
 				
-				/* Loop through the markers array and create graphics */
-				_this.markers['features'].forEach(function(marker) {
-					
-					// create a HTML element for each feature
-					var el = document.createElement('div');
-					el.className = 'marker';
-
-					// make a marker for each feature and add to the map
-					new mapboxgl.Marker(el)
-					.setLngLat(marker.geometry.coordinates)
-					.addTo(_this.map);
-				});
-
-				/* Create a user marker */
+			/* Loop through the markers array and create graphics */
+			app.markers.forEach(function(marker) {
+				
+				// create a HTML element for each feature
 				var el = document.createElement('div');
-				el.className = 'user';
+				el.className = 'marker';
 
 				// make a marker for each feature and add to the map
-				_this.userMarker = new mapboxgl.Marker(el)
-				.setLngLat([_this.longitude,_this.latitude])
-				.addTo(_this.map);
+				new mapboxgl.Marker(el)
+				.setLngLat(marker.lngLat)
+				.addTo(app.mapbox);
+			});
 
-				/* listen for geolocation updates and move the map */
-				_this.geolocationId = navigator.geolocation.watchPosition(_this.onGeolocationSuccess, _this.onGeolocationError, _this.geolocationSettings);
+			/* Create a user marker */
+			var el = document.createElement('div');
+			el.className = 'user';
 
-			});			
+			// make a marker for each feature and add to the map
+			app.userMarker = new mapboxgl.Marker(el)
+			.setLngLat(app.lngLat)
+			.addTo(app.mapbox);
 
-		},
+			/* listen for geolocation updates and move the map */
+			navigator.geolocation.watchPosition(
+				app.onGeolocationSuccess, 
+				app.onGeolocationError, 
+				app.geolocationSettings
+			);
 
-		/* AUDIO STUFF */
-
-		playAudio: function(file){
-			console.log("playAudio: " + file);
-			this.audioName = file;
-			this.audio.src = "/audio/"+file+".mp3";
-			this.audio.play();
-		},
-
-		toggleAudio: function(){
-			if (this.isPlaying){
-				this.audio.pause();
-			} else {
-				this.audio.play();
-			}
-		},
-
-		onAudioUpdate: function(){
-			this.currentTime = Math.floor(this.audio.currentTime);
-			this.totalTime = Math.floor(this.audio.duration);
-		},
-
-		onAudioStarted: function(){
-			console.log("onAudioStarted");
-			console.log(this);
-			this.isPlaying = true;
-		},
-
-		onAudioStopped: function(){
-			this.isPlaying = false;
-			console.log("onAudioStopped");
-		}
-
+		});		
+		
 	},
 
-	computed: {
+	findMarkerToPlay: function() {
 
-		progress: function(){
-			return Math.floor((this.currentTime / this.totalTime) * 100);
-		},
+		for (var i = app.markers.length - 1; i >= 0; i--) {
 
-		jsSupported: function(){
-			/* always return TRUE is javascript is running */
-			return true;
-		},
+			var marker = app.markers[i];
 
-		audioSupported: function(){
-			return Modernizr.audio;
-		},
+			/* measure the distance between the user position and each marker position */
+			var from = turf.point(app.lngLat);
+			var to = turf.point(marker.lngLat);
 
-		geolocationSupported: function(){
-			return Modernizr.geolocation;
-		},
+			/* convert km to nearest metre */				
+			var distance = Math.floor( turf.distance(from, to) * 1000 );
 
+			console.log(distance);
+
+			if ( distance <= app.config.audioDistance ) {
+				// play the audio
+				app.playAudio( marker.mp3 );
+
+				// remove the item from the array
+				app.markers.splice(i, 1);
+
+				alert("play " + marker.mp3 );
+			}
+		}
+	},
+	
+	playAudio: function(file){
+		console.log("playAudio: " + file);
+		app.audio.src = "/audio/"+file+".mp3";
+		app.audio.play();
+	},
+ 
+    onAudioUpdate: function() {
+        var currentTime = Math.floor(app.audio.currentTime);
+		var totalTime = Math.floor(app.audio.duration);
+		var progress = Math.floor((currentTime / totalTime) * 100);
+		// console.log(progress, currentTime, totalTime);
+	},
+
+	onAudioStarted: function(){
+		app.isAudioPlaying = true;
+		console.log("onAudioStarted");
+	},
+	
+	onAudioStopped: function(){
+		app.isAudioPlaying = false;
+		console.log("onAudioStopped");
 	}
-});
+};
+
+$( document ).ready( app.init );
